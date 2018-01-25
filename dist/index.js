f3364ba8c98abb45e7b1847a829abcb05da316dd
sappidashin <johnny.yeh@sapphireinfo.com.tw>
2018-01-25 18:11:15 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,10 @@ var _bluebird = require('bluebird');
 
 var _bluebird2 = _interopRequireDefault(_bluebird);
 
+var _getExchangeRate = require('./getExchangeRate');
+
+var _getExchangeRate2 = _interopRequireDefault(_getExchangeRate);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const line = require('@line/bot-sdk');
@@ -28,11 +32,38 @@ const handleEvent = event => {
 		return _bluebird2.default.resolve(null);
 	}
 
+	const txt = event.message.text;
+
+	if (!/\$/.test(txt)) return _bluebird2.default.resolve(null);
+
+	const commands = /\$(.*)/.exec(txt)[1].split(' ');
+
+	const currency = commands[0];
+
+	return (0, _getExchangeRate2.default)().then(data => {
+		const curReg = new RegExp(currency);
+		const rateInfo = data.find(item => {
+			return curReg.test(item.currency, 'i');
+		});
+
+		if (rateInfo == null) return null;
+
+		const messages = [];
+		messages.push(`現金買入: ${rateInfo.cashBuy}`);
+		messages.push(`現金賣出: ${rateInfo.cashSell}`);
+		messages.push(`即期買入: ${rateInfo.buy}`);
+		messages.push(`即期賣出: ${rateInfo.sell}`);
+
+		return client.replyMessage(event.replyToken, messages.map(item => {
+			return { type: 'text', text: item };
+		}));
+	});
+
 	// create a echoing text message
-	const echo = { type: 'text', text: event.message.text };
+	// const echo = { type: 'text', text: event.message.text };
 
 	// use reply API
-	return client.replyMessage(event.replyToken, echo);
+	// return client.replyMessage(event.replyToken, echo);
 };
 
 app.post('/callback', line.middleware(config), (req, res) => {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const line = require('@line/bot-sdk')
 import express from 'express'
 import Promise from 'bluebird'
+import getExchangeRateData from './getExchangeRate'
 
 const config = {
 	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -17,11 +18,42 @@ const handleEvent = (event) => {
 	    return Promise.resolve(null);
 	  }
 
+	  const txt = event.message.text
+
+	  if(!/\$/.test(txt))
+	  	return Promise.resolve(null)
+
+	  const commands = /\$(.*)/.exec(txt)[1].split(' ')
+
+	  const currency = commands[0]
+
+	  return getExchangeRateData()
+	  			.then(data => {
+	  				const curReg = new RegExp(currency)
+	  				const rateInfo = data.find(item => {
+	  					return curReg.test(item.currency, 'i')
+	  				})
+
+	  				if(rateInfo == null)
+	  					return null
+
+	  				const messages = []
+	  				messages.push(`現金買入: ${rateInfo.cashBuy}`)
+	  				messages.push(`現金賣出: ${rateInfo.cashSell}`)
+	  				messages.push(`即期買入: ${rateInfo.buy}`)
+	  				messages.push(`即期賣出: ${rateInfo.sell}`)
+
+	  				return client.replyMessage(event.replyToken, messages.map(item => {
+	  					return { type: 'text', text: item }
+	  				}))
+	  			})
+
+
 	  // create a echoing text message
-	  const echo = { type: 'text', text: event.message.text };
+	  // const echo = { type: 'text', text: event.message.text };
 
 	  // use reply API
-	  return client.replyMessage(event.replyToken, echo);
+	  // return client.replyMessage(event.replyToken, echo);
 }
 
 app.post('/callback', line.middleware(config), (req, res) => {
