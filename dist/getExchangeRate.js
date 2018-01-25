32fa62350016667d71df47e2b079769b479b2894
sappidashin <johnny.yeh@sapphireinfo.com.tw>
2018-01-25 18:14:17 +0800

    update

--- dist/getExchangeRate.js
+++ dist/getExchangeRate.js
@@ -12,66 +12,73 @@ var _cheerio = require('cheerio');
 
 var _cheerio2 = _interopRequireDefault(_cheerio);
 
+var _bluebird = require('bluebird');
+
+var _bluebird2 = _interopRequireDefault(_bluebird);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const targetUrl = 'http://rate.bot.com.tw/xrt?Lang=en-US';
 
 const getExchangeRateData = () => {
-	_superagent2.default.get(targetUrl).end((err, res) => {
-		if (err) {
-			throw Error(err);
-		}
+	return new _bluebird2.default((resolve, reject) => {
+		_superagent2.default.get(targetUrl).end((err, res) => {
+			if (err) {
+				throw Error(err);
+				reject(err);
+			}
 
-		let $ = _cheerio2.default.load(res.text);
+			let $ = _cheerio2.default.load(res.text);
 
-		const exchangeRateData = [];
+			const exchangeRateData = [];
 
-		$('.table.table-striped.table-bordered.table-condensed.table-hover tbody tr').each(function (i, elem) {
-			const _t = $(this);
+			$('.table.table-striped.table-bordered.table-condensed.table-hover tbody tr').each(function (i, elem) {
+				const _t = $(this);
 
-			let info = {};
+				let info = {};
 
-			_t.find('td').filter(i => i < 5).each(function (i, elem) {
-				const _td = $(this);
+				_t.find('td').filter(i => i < 5).each(function (i, elem) {
+					const _td = $(this);
 
-				let value;
-				switch (i) {
-					case 0:
-						{
-							const currency = _td.children().first().children().eq(3).text();
-							const reg = /\((\w*)\)/.exec(currency);
-							if (reg) info.currency = reg[1];
+					let value;
+					switch (i) {
+						case 0:
+							{
+								const currency = _td.children().first().children().eq(3).text();
+								const reg = /\((\w*)\)/.exec(currency);
+								if (reg) info.currency = reg[1];
+								break;
+							}
+						case 1:
+							{
+								info.cashBuy = _td.text();
+								break;
+							}
+						case 2:
+							{
+								info.cashSell = _td.text();
+								break;
+							}
+						case 3:
+							{
+								info.buy = _td.text();
+								break;
+							}
+						case 4:
+							{
+								info.sell = _td.text();
+								break;
+							}
+						default:
 							break;
-						}
-					case 1:
-						{
-							info.cashBuy = _td.text();
-							break;
-						}
-					case 2:
-						{
-							info.cashSell = _td.text();
-							break;
-						}
-					case 3:
-						{
-							info.buy = _td.text();
-							break;
-						}
-					case 4:
-						{
-							info.sell = _td.text();
-							break;
-						}
-					default:
-						break;
-				}
+					}
+				});
+
+				exchangeRateData.push(info);
 			});
 
-			exchangeRateData.push(info);
+			resolve(exchangeRateData);
 		});
-
-		return exchangeRateData;
 	});
 };
 
--- src/getExchangeRate.js
+++ src/getExchangeRate.js
@@ -1,60 +1,64 @@
 import superagent from 'superagent'
 import cheerio from 'cheerio'
+import Promise from 'bluebird'
 
 const targetUrl = 'http://rate.bot.com.tw/xrt?Lang=en-US'
 
 const getExchangeRateData = () => {
-	superagent.get(targetUrl).end((err, res) => {
-		if(err){
-			throw Error(err)
-		}
+	return new Promise((resolve, reject) => {
+		superagent.get(targetUrl).end((err, res) => {
+			if(err){
+				throw Error(err)
+				reject(err)
+			}
 
-		let $ = cheerio.load(res.text)
+			let $ = cheerio.load(res.text)
 
-		const exchangeRateData = []
+			const exchangeRateData = []
 
-		$('.table.table-striped.table-bordered.table-condensed.table-hover tbody tr').each(function(i, elem){
-			const _t = $(this)
-			
-			let info = {}
+			$('.table.table-striped.table-bordered.table-condensed.table-hover tbody tr').each(function(i, elem){
+				const _t = $(this)
+				
+				let info = {}
 
-			_t.find('td').filter(i => i < 5).each(function(i, elem){
-				const _td = $(this)
+				_t.find('td').filter(i => i < 5).each(function(i, elem){
+					const _td = $(this)
 
-				let value
-				switch(i){
-					case 0:{					
-						const currency = _td.children().first().children().eq(3).text()
-						const reg = /\((\w*)\)/.exec(currency)
-						if(reg)
-							info.currency = reg[1]
-						break
+					let value
+					switch(i){
+						case 0:{					
+							const currency = _td.children().first().children().eq(3).text()
+							const reg = /\((\w*)\)/.exec(currency)
+							if(reg)
+								info.currency = reg[1]
+							break
+						}
+						case 1:{
+							info.cashBuy = _td.text()
+							break
+						}
+						case 2:{
+							info.cashSell = _td.text()
+							break
+						}
+						case 3:{
+							info.buy = _td.text()
+							break
+						}
+						case 4:{
+							info.sell = _td.text()
+							break
+						}
+						default:
+							break
 					}
-					case 1:{
-						info.cashBuy = _td.text()
-						break
-					}
-					case 2:{
-						info.cashSell = _td.text()
-						break
-					}
-					case 3:{
-						info.buy = _td.text()
-						break
-					}
-					case 4:{
-						info.sell = _td.text()
-						break
-					}
-					default:
-						break
-				}
+				})
+
+				exchangeRateData.push(info)
 			})
 
-			exchangeRateData.push(info)
+			resolve(exchangeRateData)
 		})
-
-		return exchangeRateData
 	})
 }
 
